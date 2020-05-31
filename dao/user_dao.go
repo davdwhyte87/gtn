72e6779290c544ed5bb42ad1bdd590c54cff403a
David whyte <37925345+davdwhyte87@users.noreply.github.com>
2020-05-31 20:34:35 +0100

    Update user_dao.go

--- dao/user_dao.go
+++ dao/user_dao.go
@@ -58,6 +58,6 @@ func (m *UserDAO) Delete(user models.User) error {
 func (m *UserDAO) Update(user models.User) error {
 	docID, _ := primitive.ObjectIDFromHex(user.ID.Hex())
 	userb, _ := bson.Marshal(user)
-	_, err := UserCollection.UpdateOne(Ctx, bson.M{"_id": docID}, userb)
+	_, err := UserCollection.UpdateOne(Ctx, bson.M{"_id": docID}, bson.M{"$set":userb})
 	return err
 }
